5bb6659ba3ba68cb4de0b13291cd92385b2190a9
Turhan Can Kargin <turhancan.kargin@gmail.com>
2023-12-20 23:41:23 +0100

    Update image processing in alphabet_extractor.py

--- alphabet_extractor.py
+++ alphabet_extractor.py
@@ -1,7 +1,8 @@
 import os
-from PIL import Image, ImageFilter
+from PIL import Image, ImageOps
 import numpy as np
 import pickle
+from utils import adjust_grayscale
 
 def from_jpg_to_png(folder):
     for label in os.listdir(folder):
@@ -15,10 +16,6 @@ def from_jpg_to_png(folder):
                     img.save(img_path.replace('.jpg', '.png'))
                     os.remove(img_path)
 
-
-def convert_to_binary(image, threshold=60):
-    return image.point(lambda x: 0 if x < threshold else 255, '1')
-
 def load_images_from_folder(folder):
     images = []
     labels = []
@@ -27,15 +24,18 @@ def load_images_from_folder(folder):
         if os.path.isdir(label_path):
             for image_file in os.listdir(label_path):
                 if image_file.endswith('.png'):
+                    print(label_path, end='\r')
                     img_path = os.path.join(label_path, image_file)
-                    img = Image.open(img_path).convert('L')  # Convert to grayscale
-                    img = img.filter(ImageFilter.FIND_EDGES)  # Apply edge detection
+                    # Convert to grayscale with black background
+                    img = Image.open(img_path).convert('L')
+                    img = ImageOps.invert(img)
+                    # img = img.filter(ImageFilter.FIND_EDGES)  # Apply edge detection
                     img = img.resize((28, 28), Image.ANTIALIAS)  # Resize with a higher-quality downsampling filter
-                    img = convert_to_binary(img)  # Convert to binary image
-                    img = np.array(img).astype(int)  # from True and False to 1 and 0
-                    img_array = np.array(img).flatten()  # Flatten the image
+                    img = adjust_grayscale(img)  # Adjust grayscale
+                    # from True and False to 1 and 0 and Flatten the image
+                    img_array = np.array(img).astype(int).flatten()
                     images.append(img_array)
-                    labels.append(ord(label) - ord('a'))  # Convert label to numerical
+                    labels.append(ord(label))  # Convert label to numerical
     return np.array(images), np.array(labels)
 
 def process_and_save_data(training_dir, validation_dir, testing_dir, output_file):
