3578b68fb87f5822de7867498f9b103d1a91ce3b
Turhan Can Kargin <turhancan.kargin@gmail.com>
2023-12-20 23:41:15 +0100

    Add functions to load and save files
    This commit adds new functions to load and save files using pickle. The functions include loading MNIST data and alphabet data, as well as saving data to a pickle file.

---
+++ load_and_save_files.py
@@ -0,0 +1,18 @@
+import pickle
+
+def load_mnist():
+    with open('mnist.pkl', 'rb') as f:
+        mnist = pickle.load(f)
+    return mnist['training_images'], mnist['training_labels'], mnist['test_images'], mnist['test_labels']
+
+def load_alphabet():
+    with open('alphabet.pkl', 'rb') as f:
+        alphabet = pickle.load(f)
+        training_samples = alphabet['training_images'], alphabet['training_labels']
+        validation_samples = alphabet['validation_images'], alphabet['validation_labels']
+        test_samples = alphabet['test_images'], alphabet['test_labels']
+    return training_samples[0], training_samples[1], validation_samples[0], validation_samples[1], test_samples[0], test_samples[1]
+
+def save_pickle(data, output_file):
+    with open(output_file, 'wb') as f:
+        pickle.dump(data, f)
