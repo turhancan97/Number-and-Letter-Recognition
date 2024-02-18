eaabe14acf55ce077e86154bcd774f868f2d2167
Turhan Can Kargin <turhancan.kargin@gmail.com>
2024-02-18 18:52:57 +0100

    Refactor load_and_save_files.py to improve readability and maintainability

--- recognition/load_and_save_files.py
+++ recognition/load_and_save_files.py
@@ -1,35 +1,54 @@
 import pickle
 
+
 def load_mnist():
-    '''
+    """
     Load the MNIST data set.
-    '''
-    with open('data/mnist.pkl', 'rb') as f:
+    """
+    with open("data/mnist.pkl", "rb") as f:
         mnist = pickle.load(f)
-    return mnist['training_images'], mnist['training_labels'], mnist['test_images'], mnist['test_labels']
+    return (
+        mnist["training_images"],
+        mnist["training_labels"],
+        mnist["test_images"],
+        mnist["test_labels"],
+    )
+
 
 def load_alphabet():
-    '''
+    """
     Load the Alphanet data set.
-    '''
-    with open('data/alphabet.pkl', 'rb') as f:
+    """
+    with open("data/alphabet.pkl", "rb") as f:
         alphabet = pickle.load(f)
-        training_samples = alphabet['training_images'], alphabet['training_labels']
-        validation_samples = alphabet['validation_images'], alphabet['validation_labels']
-        test_samples = alphabet['test_images'], alphabet['test_labels']
-    return training_samples[0], training_samples[1], validation_samples[0], validation_samples[1], test_samples[0], test_samples[1]
+        training_samples = alphabet["training_images"], alphabet["training_labels"]
+        validation_samples = (
+            alphabet["validation_images"],
+            alphabet["validation_labels"],
+        )
+        test_samples = alphabet["test_images"], alphabet["test_labels"]
+    return (
+        training_samples[0],
+        training_samples[1],
+        validation_samples[0],
+        validation_samples[1],
+        test_samples[0],
+        test_samples[1],
+    )
+
 
 def save_pickle(data, output_file):
-    '''
+    """
     Save the data in a pickle file.
-    '''
-    with open(output_file, 'wb') as f:
+    """
+    with open(output_file, "wb") as f:
         pickle.dump(data, f)
 
+
 def load_pickle(input_file):
-    '''
+    """
     Load the data from the pickle file.
-    '''
-    with open(input_file, 'rb') as f:
+    """
+    with open(input_file, "rb") as f:
         data = pickle.load(f)
     return data
